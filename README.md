8681ee793441f1753827a4980ad8152efea2acb7
Tiago Daniel Jacobs <tiago@imdt.com.br>
2019-10-11 15:03:57 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # build-react-from-ng
+
+This package allow you tu use `ng build` command to build your reactjs application by injecting the node_modules/.bin/ng script
+
+
---
+++ ng-build-react.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+ARGUMENT_LIST=(
+    "configuration"
+    "output-path"
+)
+
+
+# read arguments
+opts=$(getopt \
+    --longoptions "$(printf "%s:," "${ARGUMENT_LIST[@]}")" \
+    --name "$(basename "$0")" \
+    --options "" \
+    -- "$@"
+)
+
+eval set --$opts
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --configuration)
+            configuration=$2
+            shift 2
+            ;;
+
+        --output-path)
+            outputPath=$2
+            shift 2
+            ;;
+
+        *)
+            break
+            ;;
+    esac
+done
+
+
+echo configuration: $configuration
+echo outputPath: $outputPath
+set -e
+echo "Building ..."
+npm run-script build
+
+set +e
+echo "Copying ..."
+rm -rf dist/ &> /dev/null
+mkdir dist/
+
+set -e
+mv build $outputPath
+
+echo "Ok"
